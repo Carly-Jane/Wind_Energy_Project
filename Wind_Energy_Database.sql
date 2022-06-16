e0ccb2398d00fcb198687d4474faf5ec36514010
Carly-Jane <101085083+Carly-Jane@users.noreply.github.com>
2022-06-16 22:31:47 +0800

    transform section drafted, sql document updated

--- Wind_Energy_Database.sql
+++ Wind_Energy_Database.sql
@@ -1,42 +1,42 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+﻿gi-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
 -- Link to schema: https://app.quickdatabasediagrams.com/#/d/FKoqKn
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 
 CREATE TABLE "Storm_Data" (
     "EventID" int   NOT NULL,
-    "Database" string   NOT NULL,
+    "Database" varchar   NOT NULL,
     "ID" int   NOT NULL,
-    "Date/Time" string   NOT NULL,
-    "Nearest_town" string   NOT NULL,
-    "State" string   NOT NULL,
+    "Date/Time" varchar   NOT NULL,
+    "Nearest_town" varchar   NOT NULL,
+    "State" varchar   NOT NULL,
     "Latitude" int   NOT NULL,
     "Longitude" int   NOT NULL,
-    "Comments" string   NOT NULL
+    "Comments" varchar   NOT NULL
 );
 
 CREATE TABLE "High_Impact_Outages" (
-    "State" string   NOT NULL,
-    "NSP" string   NOT NULL,
-    "Start" string   NOT NULL,
-    "Finish" string   NOT NULL,
-    "Network_Asset" string   NOT NULL,
-    "Impact" string   NOT NULL,
-    "Recall" string   NOT NULL,
-    "Status" string   NOT NULL
+    "State" varchar   NOT NULL,
+    "NSP" varchar   NOT NULL,
+    "Start" varchar   NOT NULL,
+    "Finish" varchar   NOT NULL,
+    "Network_Asset" varchar   NOT NULL,
+    "Impact" varchar   NOT NULL,
+    "Recall" varchar   NOT NULL,
+    "Status" varchar   NOT NULL
 );
 
 CREATE TABLE "VIC_Summary" (
-    "ID" string   NOT NULL,
-    "Name" string   NOT NULL,
-    "State" string   NOT NULL,
-    "Source_Technology" string   NOT NULL,
+    "ID" varchar   NOT NULL,
+    "Name" varchar   NOT NULL,
+    "State" varchar   NOT NULL,
+    "Source_Technology" varchar   NOT NULL,
     "Registered_Capacity" int   NOT NULL
 );
 
 CREATE TABLE "States_Summary" (
-    "State" string   NOT NULL,
-    "Acronym" string   NOT NULL
+    "State" varchar   NOT NULL,
+    "Acronym" varchar   NOT NULL
 );
 
 ALTER TABLE "Storm_Data" ADD CONSTRAINT "fk_Storm_Data_State" FOREIGN KEY("State")
